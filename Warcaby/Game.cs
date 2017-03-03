c702e7cc004710df0b056294a42f89ab0f7285a7
arveduil <pboszczyk@gmail.com>
2017-03-03 10:25:38 +0100

    Warcaby
    Creating project.

---
+++ Warcaby/Game.cs
@@ -0,0 +1,487 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+
+
+namespace Warcaby
+{
+    public class Game 
+    {
+        public bool tura;
+        public MainWindow o;
+        public int selectedW;
+        public int selectedK;
+        public bool selectedQ;
+        public Game(MainWindow win)
+        {
+            this.o = win;
+            selectedK = -1;
+            selectedW = -1;
+            tura = new bool();
+            tura = true;
+            selectedQ = false;
+           
+           
+        }
+
+        public void click(int w, int k)
+        {
+            
+            if (o.pos[w, k] == -1)
+                return;
+            
+            
+           
+            
+            if(tura)
+            {
+                //wybor czarnych powoduje return;
+                if(o.pos[w,k]==2  || o.pos[w,k]==4)
+                {
+                    return;
+                }
+
+                //select
+                if(o.pos[w,k]==1 || o.pos[w,k]==3)
+                {
+                    selectedW = w;
+                    selectedK = k;
+                    if (o.pos[w, k] == 3)
+                    selectedQ = true;
+                    return;
+
+                }
+              
+                //dzialanie damki
+                if (selectedQ)
+                {
+                    whiteQSelected(w, k);
+                    return;
+                }
+                //normalne przesuniecie
+                if (((w==selectedW-1 && k==selectedK-1) ||(w==selectedW-1 && k==selectedK+1)  ) && o.pos[w,k]==0 && o.pos[selectedW,selectedK]!=0)
+                {
+                    move(selectedW, selectedK, w, k);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = false;
+                    finish();
+                    return;
+                }
+
+                //bicie
+                if((w == selectedW - 2 && k == selectedK - 2) &&( o.pos[selectedW-1,selectedK-1]==2 || o.pos[selectedW - 1, selectedK - 1] == 4))
+                {
+                    move(selectedW, selectedK, w, k);
+                    beat(selectedW - 1, selectedK - 1);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = false;
+                    finish();
+                    return;
+                }
+                //bicie2
+                if ((w == selectedW - 2 && k == selectedK +2) && (o.pos[selectedW - 1, selectedK +1] == 2 || o.pos[selectedW - 1, selectedK + 1] == 4))
+                {
+                    move(selectedW, selectedK, w, k);
+                    beat(selectedW - 1, selectedK+1);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = false;
+                    finish();
+                    return;
+                }
+
+                //bicie3
+                if ((w == selectedW + 2 && k == selectedK + 2) && (o.pos[selectedW + 1, selectedK + 1] == 2 || o.pos[selectedW + 1, selectedK + 1] == 4))
+                {
+                    move(selectedW, selectedK, w, k);
+                    beat(selectedW + 1, selectedK + 1);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = false;
+                    finish();
+                    return;
+                }
+                //bicie4
+                if ((w == selectedW + 2 && k == selectedK - 2) && (o.pos[selectedW + 1, selectedK - 1] == 2 || o.pos[selectedW + 1, selectedK - 1] == 4))
+                {
+                    move(selectedW, selectedK, w, k);
+                    beat(selectedW + 1, selectedK - 1);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = false;
+                    finish();
+                    return;
+                }
+
+
+            }
+            //czarni
+            if (!tura)
+            {
+                //klik na biale
+                if (o.pos[w, k] == 1 || o.pos[w, k] == 3)
+                {
+                    return;
+                }
+
+                //select
+                if (o.pos[w, k] == 2 || o.pos[w, k] == 4)
+                {
+                    selectedW = w;
+                    selectedK = k;
+                    if (o.pos[w, k] == 4)
+                        selectedQ = true;
+                    return;
+
+                }
+             
+                //zaznaczanie
+                if (selectedK == -1 && selectedW == -1)
+                {
+                    selectedW = w;
+                    selectedK = k;
+                    return;
+                }
+                //ruch
+                if (((w == selectedW +1 && k == selectedK +1) || (w == selectedW + 1 && k == selectedK - 1)) && ( o.pos[w, k] == 0 && o.pos[selectedW,selectedK]!=0))
+                {
+                    move(selectedW, selectedK, w, k);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = true;
+                    finish();
+                    return;
+                }
+                //dzialanie damki w drugiej metodzie
+                if (selectedQ)
+                {
+                    blackQSelected(w,k);
+                    return;
+                }
+
+
+                //zbijanie bialych
+                if ((w == selectedW + 2 && k == selectedK + 2) &&( o.pos[selectedW + 1, selectedK + 1] == 1 || o.pos[selectedW + 1, selectedK + 1] == 3))
+                {
+                    move(selectedW, selectedK, w, k);
+                    beat(selectedW +1, selectedK + 1);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = true;
+                    finish();
+                    return;
+                }
+                //bicie2
+                if ((w == selectedW + 2 && k == selectedK - 2) && (o.pos[selectedW + 1, selectedK - 1] == 1 || o.pos[selectedW + 1, selectedK - 1] == 3))
+                {
+                    move(selectedW, selectedK, w, k);
+                    beat(selectedW + 1, selectedK - 1);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = true;
+                    finish();
+                    return;
+                }
+
+                //bicie3
+                if ((w == selectedW - 2 && k == selectedK - 2) && (o.pos[selectedW - 1, selectedK - 1] == 1 || o.pos[selectedW - 1, selectedK - 1] == 3))
+                {
+                    move(selectedW, selectedK, w, k);
+                    beat(selectedW - 1, selectedK - 1);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = true;
+                    finish();
+                    return;
+                }
+                //bicie4
+                if ((w == selectedW - 2 && k == selectedK + 2) && (o.pos[selectedW - 1, selectedK + 1] == 1|| o.pos[selectedW - 1, selectedK + 1] == 3))
+                {
+                    move(selectedW, selectedK, w, k);
+                    beat(selectedW - 1, selectedK + 1);
+                    selectedK = -1;
+                    selectedW = -1;
+                    tura = true;
+                    finish();
+                    return;
+                }
+
+
+            }
+
+
+
+        }
+
+        public void whiteQSelected(int w, int k)
+        {
+            bool isEnenmy = false;
+            //wspolrzedne przeciwnika (jezeli to bicie)
+            int enW, enK;
+            enW = 0;
+            enK = 0;
+            int i = 0;
+
+            //aby zablokować ruchy poziome i pionowe
+            if (selectedW == w || selectedK == k)
+                return;
+
+            //decyzja co zrobic z ruchem
+            if (w<selectedW && k<selectedK)
+                   while(true) 
+                {
+                    i++;
+                    if (o.pos[selectedW - i, selectedK - i] == 1 || o.pos[selectedW - i, selectedK - i] == 3)
+                        return;
+                    if(o.pos[selectedW - i, selectedK - i] ==2|| o.pos[selectedW - i, selectedK - i] ==4)
+                    {
+                        if (isEnenmy) return;
+                        isEnenmy = true;
+                        enW = selectedW - i;
+                        enK = selectedK - i;
+                    }
+                    if (selectedW - i == w && selectedK -i == k)
+                        break;
+
+                }
+            if (w > selectedW && k < selectedK)
+                while (true)
+                {
+                    i++;
+                    if (o.pos[selectedW + i, selectedK - i] == 1 || o.pos[selectedW + i, selectedK - i] == 3)
+                        return;
+                    if (o.pos[selectedW + i, selectedK - i] == 2 || o.pos[selectedW + i, selectedK - i] == 4)
+                    {
+                        if (isEnenmy) return;
+                        isEnenmy = true;
+                        enW = selectedW +i;
+                        enK = selectedK - i;
+                    }
+                    if (selectedW + i == w && selectedK - i == k)
+                        break;
+
+                }
+            if (w > selectedW && k > selectedK)
+                while (true)
+                {
+                    i++;
+                    if (o.pos[selectedW + i, selectedK + i] == 1 || o.pos[selectedW + i, selectedK + i] == 3)
+                        return;
+                    if (o.pos[selectedW + i, selectedK + i] == 2 || o.pos[selectedW + i, selectedK + i] == 4)
+                    {
+                        if (isEnenmy) return;
+                        isEnenmy = true;
+                        enW = selectedW + i;
+                        enK = selectedK + i;
+                    }
+                    if (selectedW + i == w && selectedK + i == k)
+                        break;
+
+                }
+            if (w <selectedW && k > selectedK)
+                while (true)
+                {
+                    i++;
+                    if (o.pos[selectedW - i, selectedK + i] == 1 || o.pos[selectedW - i, selectedK +i] == 3)
+                        return;
+                    if (o.pos[selectedW - i, selectedK + i] == 2 || o.pos[selectedW - i, selectedK +i] == 4)
+                    {
+                        if (isEnenmy) return;
+                        isEnenmy = true;
+                        enW = selectedW - i;
+                        enK = selectedK + i;
+                    }
+                    if (selectedW - i == w && selectedK + i == k)
+                        break;
+
+                }
+
+            if (isEnenmy)
+            {
+                beat(enW, enK);
+                
+            }
+            move(selectedW, selectedK, w, k);
+            selectedQ = false;
+            selectedW = -1;
+            selectedK = -1;
+           
+            tura = false;
+            finish();
+            
+        }
+
+        //analogicznie do bialej
+        public void blackQSelected(int w, int k)
+        {
+            bool isEnenmy = false;
+            int enW, enK;
+            enW = 0;
+            enK = 0;
+            int i = 0;
+
+            //aby zablokować ruchy poziome i pionowe
+            if (selectedW == w || selectedK == k)
+                return;
+
+            //poszukiwania cwiartki
+            if (w < selectedW && k < selectedK)
+                while (true)
+                {
+                    i++;
+                    
+                    if (o.pos[selectedW - i, selectedK - i] == 2 || o.pos[selectedW - i, selectedK - i] == 4)
+                        return;
+                    if (o.pos[selectedW - i, selectedK - i] == 1 || o.pos[selectedW - i, selectedK - i] == 3)
+                    {
+                        if (isEnenmy) return;
+                        isEnenmy = true;
+                        enW = selectedW - i;
+                        enK = selectedK - i;
+                    }
+                    if (selectedW - i == w && selectedK - i == k)
+                        break;
+
+                }
+            if (w > selectedW && k < selectedK)
+                while (true)
+                {
+                    i++;
+                    if (o.pos[selectedW + i, selectedK - i] == 2|| o.pos[selectedW + i, selectedK - i] == 4)
+                        return;
+                    if (o.pos[selectedW + i, selectedK - i] == 1 || o.pos[selectedW + i, selectedK - i] == 3)
+                    {
+                        if (isEnenmy) return;
+                        isEnenmy = true;
+                        enW = selectedW + i;
+                        enK = selectedK - i;
+                    }
+                    if (selectedW + i == w && selectedK - i == k)
+                        break;
+
+                }
+            if (w > selectedW && k > selectedK)
+                while (true)
+                {
+                    i++;
+                    if (o.pos[selectedW + i, selectedK + i] == 2 || o.pos[selectedW + i, selectedK + i] == 4)
+                        return;
+                    if (o.pos[selectedW + i, selectedK + i] == 1|| o.pos[selectedW + i, selectedK + i] == 3)
+                    {
+                        if (isEnenmy) return;
+                        isEnenmy = true;
+                        enW = selectedW + i;
+                        enK = selectedK + i;
+                    }
+                    if (selectedW + i == w && selectedK + i == k)
+                        break;
+
+                }
+            if (w < selectedW && k > selectedK)
+                while (true)
+                {
+                    i++;
+                    if (o.pos[selectedW - i, selectedK + i] == 2 || o.pos[selectedW - i, selectedK + i] == 4)
+                        return;
+                    if (o.pos[selectedW - i, selectedK + i] == 1 || o.pos[selectedW - i, selectedK + i] == 3)
+                    {
+                        if (isEnenmy) return;
+                        isEnenmy = true;
+                        enW = selectedW - i;
+                        enK = selectedK + i;
+                    }
+                    if (selectedW - i == w && selectedK + i == k)
+                        break;
+
+                }
+
+            if (isEnenmy)
+            {
+                beat(enW, enK);
+
+            }
+            move(selectedW, selectedK, w, k);
+            selectedQ = false;
+            selectedW = -1;
+            selectedK = -1;
+         
+            tura = true;
+            finish();
+
+        }
+
+
+        //przenies figurem i zmien tlo
+        public void move(int prevW, int prevK, int curW, int curK)
+        {
+            int what = o.pos[prevW, prevK];
+            o.pos[prevW, prevK] = 0;
+            o.makeEmpty(prevW, prevK);
+            // selectedK = -1;
+            //selectedW = -1;
+            o.pos[curW, curK] = what;
+            if(what==1)
+            o.makeWhiteP(curW, curK);
+            if (what == 2)
+                o.makeBlackP(curW, curK);
+            if (what == 3)
+                o.makeWhiteQ(curW, curK);
+            if (what == 4)
+                o.makeBlackQ(curW, curK);
+        }
+        //zbij
+        public void beat(int W,int K)
+        {
+            o.pos[W, K] = 0;
+            o.makeEmpty(W, K);
+        }
+
+
+        
+        public void finish()
+        {
+            int whiteCounter = 0;
+            int blackCounter = 0;
+            //sprawdza czy dodac damke
+            for(int i=0;i<8;i++)
+                for(int j=0;j<8;j++)
+                {
+                    if (o.pos[i, j] == 1 || o.pos[i, j] == 3)
+                        whiteCounter++;
+                    if (o.pos[i, j] == 2 || o.pos[i, j] == 4)
+                        blackCounter++;
+                }
+            
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (o.pos[0, i] == 1)
+                {
+                    o.pos[0, i] = 3;
+                    o.makeWhiteQ(0,i);
+                }
+
+                if (o.pos[7,i] == 2)
+                {
+                    o.pos[7, i] = 4;
+                    o.makeBlackQ(7, i);
+                }
+            }
+            if (tura)
+                o.lab.Content = "Teraz ruch białych.";
+            else
+                o.lab.Content = "Teraz ruch czarnych.";
+
+if (whiteCounter == 0)
+                o.lab.Content = "CZARNI WYGRALI!!!";
+
+            if (blackCounter == 0)
+                o.lab.Content = "BIALI WYGRALI!!!";
+        }
+    }
+}
---
+++ Warcaby/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warcaby
+{
+    //Wartosc pol:
+    //-1 pole biale
+    //0 puste pole czarne
+    //1 bialy pion
+    //2 czarny pion
+    //3 biala damka
+    //4 czarna damka
+ 
+    //Ja jestem ta osoba co sie pytala o tlo buttonow, dlatego u mmnie jest ta glupia zamiana  na niebieskie tlo
+    public partial class MainWindow : Window
+    {
+       
+        public int[,] pos;
+        public Button[,] fields;
+        public Label lab;
+        public SolidColorBrush blackBrush= new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+        public Game game;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Wczytuje pola
+            fields = new Button[8,8] {
+                { field00,  field01, field02, field03, field04, field05, field06, field07},
+                { field10,  field11, field12, field13, field14, field15, field16, field17},
+                { field20,  field21, field22, field23, field24, field25, field26, field27},
+                { field30,  field31, field32, field33, field34, field35, field36, field37},
+                { field40,  field41, field42, field43, field44, field45, field46, field47},
+                { field50,  field51, field52, field53, field54, field55, field56, field57},
+                { field60,  field61, field62, field63, field64, field65, field66, field67},
+                { field70,  field71, field72, field73, field74, field75, field76, field77},
+            };
+         
+            pos = new int[8, 8];
+            //zeruje pozycje wszystkich
+            for(int i=0;i<8;i++)
+                for(int j=0;j<8;j++)
+                {
+                
+                    pos[i, j] = -1;
+                }
+            placeBlack();
+            placeWhite();
+            makeBlackFields();
+            lab = label;
+            game = new Game(this);
+
+
+        }
+
+
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            Button chosen = (Button)sender;
+          
+
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                    if (chosen == fields[i, j]) game.click(i, j);
+
+       }
+
+       
+        
+        public void placeBlack()
+        {
+            for(int i=0;i<3;i++)
+                for(int j=0;j<8;j++)
+                {
+                    if(i%2==0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            fields[i, j].Background = blackPawn();
+                        
+                            pos[i, j] = 2;
+                           // pos[i, j] = blackP;
+
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 1)
+                        {
+                            fields[i, j].Background = blackPawn();
+                            // blackPos[i, j] = blackP;
+                            pos[i, j] = 2;
+                        }
+                    }
+                }
+        }
+        //zrob czarne pola posrodku
+        public void makeBlackFields()
+        {
+            for (int i = 3; i < 5; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            fields[i, j].Background = blackBrush;
+                            pos[i, j] = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 1)
+                        {
+                            fields[i, j].Background = blackBrush;
+                            pos[i, j] = 0;
+                        }
+                    }
+                }
+        }
+        
+        public void placeWhite()
+        {
+            for (int i = 5; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            fields[i, j].Background = whitePawn();
+                           
+                            pos[i, j] = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 == 1)
+                        {
+                            fields[i, j].Background = whitePawn();
+                         
+                            pos[i, j] = 1;
+                        }
+                    }
+                }
+        }
+
+        public ImageBrush blackPawn()
+        {
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "\\images\\czarPion.png"));
+            return myBrush;
+        }
+
+        public ImageBrush whitePawn()
+        {
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "\\images\\bialpion.png"));
+            return myBrush;
+        }
+        public ImageBrush czarQueen()
+        {
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "\\images\\czarqueen.png"));
+            return myBrush;
+        }
+        public ImageBrush whiteQueen()
+        {
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource = new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "\\images\\bialqueen.png"));
+            return myBrush;
+        }
+
+
+        //metody ponizej zmieniaja tla buttonow
+        public void makeEmpty(int i, int j)
+        {
+            fields[i, j].Background = blackBrush;
+        }
+        public void makeWhiteP(int i, int j)
+        {
+            fields[i, j].Background = whitePawn();
+        }
+        public void makeBlackP(int i, int j)
+        {
+            fields[i, j].Background = blackPawn();
+        }
+        public void makeWhiteQ(int i, int j)
+        {
+            fields[i, j].Background = whiteQueen();
+        }
+        public void makeBlackQ(int i, int j)
+        {
+            fields[i, j].Background = czarQueen();
+        }
+
+        private void mouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
